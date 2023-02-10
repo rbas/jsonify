88971092a2007e7b42697ec29b4b81124cd108fd
Martin Voldřich <rbas.cz@gmail.com>
2023-02-10 15:30:10 +0100

    Handling json parse error

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 test:
 	cargo build
 	cat test.json |RUST_BACKTRACE=1 ./target/debug/jsonify 
+	echo "Blah" |RUST_BACKTRACE=1 ./target/debug/jsonify 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::io::{self, Read};
 
-use serde_json::{to_string_pretty, Value};
+use serde_json::{to_string_pretty, Error, Value};
+
+fn print_parsed_json(json_data: &Value) {
+    // TODO: Hande parse errors
+    let output = to_string_pretty(&json_data);
+
+    println!("{}", output.unwrap());
+}
+
+fn print_parse_error_message(error: Error) {
+    eprintln!("{:?}", error)
+}
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
@@ -9,12 +20,11 @@ fn main() -> io::Result<()> {
     // TODO: Handle invalid buffer
     stdin.read_to_string(&mut buffer)?;
 
-    let json_data: Value = serde_json::from_str(&buffer).unwrap();
-
-    // TODO: Hande parse errors
-    let output = to_string_pretty(&json_data);
-
-    println!("{}", output.unwrap());
+    let parsed_json: Result<_, serde_json::Error> = serde_json::from_str(&buffer);
+    match parsed_json {
+        Ok(parsed_json) => print_parsed_json(&parsed_json),
+        Err(e) => print_parse_error_message(e),
+    }
 
     Ok(())
 }
